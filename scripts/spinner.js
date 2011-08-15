30cf75a8d22e29c21f42497b516b32c330f3afbe
Jensen Hussey <jensechu@gmail.com>
2011-08-15 18:41:02 -0400

    Rotating a SVG path to create spaces

--- scripts/spinner.js
+++ scripts/spinner.js
@@ -4,9 +4,29 @@ $(function() {
     
     // Main circle path
     var circle = paper.circle(420, 420, 400).attr({
-	fill: "white",
-	stroke: "red",
+	//fill: "white",
+	//stroke: "red",
     });
+
+    // Spots
+    var spot = paper.path("M0 420L840 420").attr({
+	fill: "white",
+	stroke: "green",
+    });
+    
+    //Spot rotation
+    function spot_rotator(set){
+	i = 0
+	while (i<90) {
+	    set.push(
+		spot.clone().rotate(i)
+	    );
+	    i += 22.5;
+	}
+	return set;
+    }
+
+    var red_spots = spot_rotator(paper.set()).attr({stroke: "red", "stroke-width": 10});
 
     // Quadrant lines
     var xaxis = paper.path("M420 0L420 840").attr({
@@ -14,5 +34,4 @@ $(function() {
 	"stroke-width": 3
     });
     var yaxis = xaxis.clone().rotate(90);
-    
 })
